ece32157c886047dc41699e3306405da037d865d
yiyu <zhong_yi_yu@outlook.com>
2021-03-23 15:32:31 +0000

    Add in WYSIWYG note editor.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,83 @@
 <template>
   <div>
-    <mavon-editor language="en" placeholder="Please Enter your notes" />
+    <el-tiptap
+
+        :extensions="extensions"
+        output="json"
+        :content="content"
+        @onUpdate="onUpdate"
+    />
+
+    <br>
+    <h1>BELOW IS STRUCTURED HTML OUTPUT</h1>
+    <br>
+    <pre><code>{{ output.html }}</code></pre>
   </div>
 </template>
 
 <script>
+import {
+  Doc,
+  Text,
+  Paragraph,
+  Heading,
+  Bold,
+  Underline,
+  Italic,
+  Strike,
+  ListItem,
+  BulletList,
+  OrderedList,
+  TodoItem,
+  TodoList, // use with TodoItem
+  TextAlign,
+  Indent,
+  HorizontalRule,
+  HardBreak,
+  History,
+  Fullscreen,
+
+} from 'element-tiptap';
 export default {
   name: "Editor",
+  methods:{
+    onUpdate(output,options){
+      const {getJSON,getHTML} = options;
+      this.output.json = getJSON();
+      this.output.html = getHTML();
+    },
+  },
+  data(){
+    return {
+      output:{
+        json:`json content`,
+        html:`html content`
+      },
+      extensions: [
+        new Doc(),
+        new Text(),
+        new Paragraph(),
+        new Heading({ level: 5 }),
+        new Bold({ bubble: true }), // render command-button in bubble menu.
+        new Underline({ bubble: true, menubar: false }), // render command-button in bubble menu but not in menubar.
+        new Italic(),
+        new Strike(),
+        new TextAlign(),
+        new ListItem(),
+        new BulletList(),
+        new OrderedList(),
+        new TodoItem(),
+        new TodoList(),
+        new Indent(),
+        new HardBreak(),
+        new HorizontalRule({ bubble: true }),
+        new Fullscreen(),
+        new History(),
+      ],
+      // editor's content
+      content: `Enter your notes here.`,
+    };
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import router from './router'
 import Vuex from 'vuex'
 import './editor_config'
 import './plugins/element.js'
+import 'element-tiptap/lib/index.css';
+import {ElementTiptapPlugin} from "element-tiptap";
 Vue.config.productionTip = true
 Vue.use(Vuex)
+Vue.use(ElementTiptapPlugin,{lang:"en",spellcheck:true})
 new Vue({
   router,
   render: h => h(App)
