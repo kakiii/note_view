40f068633e100b1aeba3fd701b6646b835387b1f
Zhong Yiyu <53254770+zhong-yi-yu@users.noreply.github.com>
2021-03-30 10:21:49 +0100

    Patch 2

--- src/components/Gravatar2.vue
+++ src/components/Gravatar2.vue
@@ -26,8 +26,8 @@ export default {
 };
 
 function avatar(gen_key) {
-  if(gen_key.length==0){
-      return null;
+  if (gen_key.length == 0) {
+    return null;
   }
   var hash = md5(gen_key);
   var str = hash.toString(16);
@@ -36,12 +36,52 @@ function avatar(gen_key) {
   var svg = "<svg>";
   for (var i = 0; i < 8; i++) {
     for (var j = 0; j < 4; j++) {
-      if (str.charAt(4*i+j) < 8) {
-            svg += "<rect x='" + size*j + "' y='" + size*i + "' width='" + size + "' height='" + size + "' style='fill:white'/>";
-            svg += "<rect x='" + size*(7-j) + "' y='" + size*i + "' width='" + size + "' height='" + size + "' style='fill:white'/>";
+      if (str.charAt(4 * i + j) < 8) {
+        svg +=
+          "<rect x='" +
+          size * j +
+          "' y='" +
+          size * i +
+          "' width='" +
+          size +
+          "' height='" +
+          size +
+          "' style='fill:white'/>";
+        svg +=
+          "<rect x='" +
+          size * (7 - j) +
+          "' y='" +
+          size * i +
+          "' width='" +
+          size +
+          "' height='" +
+          size +
+          "' style='fill:white'/>";
       } else {
-            svg += "<rect x='" + size*j + "' y='" + size*i + "' width='" + size + "' height='" + size + "' style='fill:" + color + "'/>";
-            svg += "<rect x='" + size*(7-j) + "' y='" + size*i + "' width='" + size + "' height='" + size + "' style='fill:" + color + "'/>";
+        svg +=
+          "<rect x='" +
+          size * j +
+          "' y='" +
+          size * i +
+          "' width='" +
+          size +
+          "' height='" +
+          size +
+          "' style='fill:" +
+          color +
+          "'/>";
+        svg +=
+          "<rect x='" +
+          size * (7 - j) +
+          "' y='" +
+          size * i +
+          "' width='" +
+          size +
+          "' height='" +
+          size +
+          "' style='fill:" +
+          color +
+          "'/>";
       }
     }
   }
