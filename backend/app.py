dba3f2170953f741caaec9a444dccec2dbfb3928
Zhong Yiyu <53254770+zhong-yi-yu@users.noreply.github.com>
2021-03-31 11:11:38 +0100

    Add in Flask-RESTful plugin.

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,38 @@
 import bcrypt
-from flask import Flask, render_template, json, jsonify, request, redirect, url_for, session
-# from flask_pymongo import PyMongo
-
 import pymongo
-app = Flask(__name__, static_folder="../dist/static", template_folder="../dist")
+from flask import Flask, json, jsonify, request, redirect, url_for, session
+from flask_restful import Resource, Api
 
+# from flask_pymongo import PyMongo
+app = Flask(__name__, static_folder="../dist/static", template_folder="../dist")
+api = Api(app)
 # 实际部署时需要更改
 # 数据库的文件在account.json里面
 
-client = pymongo.MongoClient("mongodb+srv://zhongyiyu:Zhongyiyu123!@note-view.cfr3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://zhongyiyu:Zhongyiyu123!@note-view.cfr3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 database = client.account
 
 
+class Book(Resource):
+    def get(self, book_id):
+        print(book_id)
+        articles = database.articles
+        match_article = articles.find_one({'id': book_id})
+        if match_article:
+            return jsonify({'ID': book_id, 'Content': match_article['content']})
+        else:
+            return jsonify(({'ID': book_id, 'Content': 'NOT FOUND'}))
+
+
+api.add_resource(Book, '/book/<int:book_id>')
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return "<h1>h1</h1>"
 
 
 @app.route('/weather')
@@ -37,13 +54,13 @@ def login():
         if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             print("Check complete")
-            return redirect(url_for('index'))
+            return jsonify({'status': 'success', 'user': login_user['username']})
         else:
             print("EXISTS")
             print(request.form['password'])
             print("\n\n")
             print(login_user['password'])
-            return jsonify({'status': 'exists'})
+            return jsonify({'status': 'error'})
     else:
         return jsonify({'status': 'failure', 'user': request.form['username']})
 
@@ -66,18 +83,9 @@ def articles():
     return "<h1>hoho</h1>"
 
 
-@app.route('/articles/<int:article_id>', methods=['GET'])
-def return_article(article_id):
-    print(article_id)
-    articles = database.articles
-    match_article = articles.find_one({'id': article_id})
-    if match_article:
-        return jsonify({'ID': article_id, 'Content': match_article['content']})
-    else:
-        return jsonify(({'ID': article_id, 'Content': 'NOT FOUND'}))
-
+# @app.route('/articles/<int:article_id>', methods=['GET'])
 
 if __name__ == "__main__":
     # 确保传输安全的密钥，可以随意更换
     app.secret_key = 'testing'
-    app.run(debug=True, port=10010)
+    app.run(debug=True, port=5000)
