cd65b261ae0e5508858e37c0423c494346a04cbb
sfyzsr <56057506+sfyzsr@users.noreply.github.com>
2021-04-11 16:18:16 +0100

    Add files via upload
    删除了discussion一些注释
    新作了todo

--- src/components/Discussion.vue
+++ src/components/Discussion.vue
@@ -5,26 +5,6 @@
     <el-container>
       <el-header>This is discussion borad. 这是讨论版</el-header>
 
-      <!-- 
-    <el-container>
-      <el-main
-        ><ul
-          class="infinite-list"
-          ref="scroll"
-          v-infinite-scroll="handleScroll"
-          infinite-scroll-disabled="disabled"
-          style="overflow: auto"
-        >
-          <li v-for="i in count" :key="i" class="infinite-list-item">
-            {{ i }}
-          </li>
-        </ul>
-
-          <p v-if="loading">Loading...</p>
-       <p v-if="noMore">No more...</p>
-
-      </el-main>-->
-
       <el-main>
         <li class="content" v-for="i in 8" :key="i" style="overflow: auto">
           {{ "this is " + i + "th comment" }}
@@ -71,58 +51,6 @@ export default {
 };
 </script>
 
-<!-- 
-<script>
-export default {
-  name: "Discussion",
-  data() {
-    return {
-      count: 5,
-      loading: false,
-    };
-  },
-  computed: {
-    /*noMore() {
-      return this.count >= 30;
-    },*/
-    disabled() {
-      //return this.loading || this.noMore;
-      return false || this.loading;
-    },
-  },
-  mounted() {
-    var that = this;
-    document
-      .getElementsByClassName("content")[0]
-      .addEventListener("scroll", that.handleScroll, true);
-  },
-  methods: {
-    handleScroll() {
-      var that = this;
-      var scrollHeight = that.$refs["scrolling"].scrollHeight;
-      var scrollTop = that.$refs["scrolling"].scrollTop;
-      var clientHeight = that.$refs["scrolling"].clientHeight;
-
-      console.log("Scrollheight is " + scrollHeight);
-      console.log("scrollTop is " + scrollTop);
-      console.log("client height is " + clientHeight);
-
-      if (scrollTop + clientHeight >= scrollHeight) {
-        this.count += 5;
-      }
-    },
-
-    /*load() {
-      this.loading = true;
-      setTimeout(() => {
-        this.count += 2;
-        this.loading = false;
-      }, 100);
-    },*/
-  },
-};
-</script>
--->
 
 <style>
 .el-header {
@@ -146,13 +74,5 @@ export default {
   line-height: 60px;
 }
 </style>
-
-<!--<script>
-import Login from "@/components/Login";
-export default {
-  name: "Discussion",
-  components: { Login },
-};
-</script>-->
 
 <style scoped></style>
