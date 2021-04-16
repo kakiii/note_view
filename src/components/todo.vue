e3a815cb743123de9c823ac9989113a60be0aee9
sfyzsr <56057506+sfyzsr@users.noreply.github.com>
2021-04-16 23:39:46 +0100

    Add files via upload
    使用localstorage进行本地存储

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -2,6 +2,11 @@
 
 <template>
   <div>
+
+    <!--测试用
+    <p>Local Data = {{ st }}</p>
+    <button @click="myGetFunction()">Get Local Storage Data</button><br />
+-->
     <h2>To Do List</h2>
     <input
       type="text"
@@ -13,7 +18,8 @@
 
     <!-- ToDo -->
     <h3>ToDo</h3>
-    <ul v-for="(item, index) in list" :key="index">
+    <ul v-for="(item, index) in list" :key="'info-' + index">
+      <!-- 这里的info没什么意义就是防俩v-for并行报错-->
       <li v-if="item.done === false">
         <input type="checkbox" @change="change(index, true)" />
         <span>{{ item.text }}</span>
@@ -34,22 +40,44 @@
         <button @click="remove(index)">delete</button>
       </li>
     </ul>
+
+
   </div>
 </template>
 
 <script>
+function set(key, val) {
+  localStorage.setItem(key, JSON.stringify(val));
+}
+function get(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+var myStorage = {
+  set: set,
+  get: get,
+};
 export default {
   name: "todo",
+
   data() {
     return {
       inputValue: "",
       list: [],
+      st: "",
+
+      //mylist: localStorage.getItem("myitem"),
     };
   },
   methods: {
     add() {
-      this.list.push({ text: this.inputValue, done: false });
-      this.inputValue = null;
+      if (this.inputValue == null) {
+        alert("不能爲空");
+      } else {
+        //this.list.push({ text: this.inputValue, done: false });
+        this.list.push({ text: this.inputValue, done: false });
+
+        this.inputValue = null;
+      }
     },
     change(index, done) {
       if (done) {
@@ -61,6 +89,30 @@ export default {
     remove(index) {
       this.list.splice(index, 1);
     },
+
+/*测试用
+    myGetFunction: function () {
+      this.st = myStorage.get("list");
+      console.log("hello" + this.st);
+    },*/
+
+  },
+
+
+  watch: {
+    list: {
+      deep: true,
+      handler: function (newVal, oldVal) {
+        if (newVal) {
+          myStorage.set("list", newVal);
+        } else {
+          myStorage.set("list", oldVal);
+        }
+      },
+    },
+  },
+  mounted: function () {
+    this.list = myStorage.get("list")|| [];
   },
 };
 </script>
