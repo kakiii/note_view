d6135ac677e7f98c28411a89d801a46e27b1da18
Zhong Yiyu <53254770+zhong-yi-yu@users.noreply.github.com>
2021-04-16 10:08:45 +0100

    tmp change

--- backend/app.py
+++ backend/app.py
@@ -21,8 +21,8 @@ app.register_blueprint(auth)
 
 @app.route('/')
 def index():
-    # return "<h1>SUCCESS</h1>"
-    return render_template("index.html")
+    return "<h1>SUCCESS</h1>"
+    # return render_template("index.html")
 
 
 # @app.route('/content/login', methods=['POST'])
