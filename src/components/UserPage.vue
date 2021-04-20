f81e07718dd7813cd8b8bacdc2980e771169da03
Zhong Yiyu <53254770+zhong-yi-yu@users.noreply.github.com>
2021-04-20 02:20:25 +0100

    Add in user page's auto loading information function.

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -6,13 +6,54 @@
 
     <Gravatar :gen_key=this.$store.state.username
               style="margin: auto"/>
+
+    <input type="button" value="GET ARTCILE" v-on:click="get_my_article"/>
+    <p>User's article collection in array form.</p>
+<!--    之后其他人把这个东西改成v-for的形式-->
+<!--    等待增加具体的美化-->
+<!--    使用mounted，在user page加载的时候就能自动获取后端用户的数据-->
+
+    <p>{{article_list}}</p>
   </div>
 </template>
 
 <script>
 import Gravatar from "./Gravatar";
-
+import axios from "axios";
 export default {
   components: {Gravatar},
+  data(){
+    return{
+      article_list:[],
+    }
+  },
+  methods: {
+    get_my_article() {
+      let url = "";
+      if (process.env.NODE_ENV === "development") {
+        url = "http://localhost:5000/article/user/";
+      } else {
+        url = "article/user/";
+      }
+      axios.get(url + this.$store.state.username).then((res) => {
+        let arr = res.data["article_collection"]
+        console.log(arr);
+        this.article_list = arr;
+      }).catch((err) => console.log(err));
+    }
+  },
+  mounted() {
+      let url = "";
+      if (process.env.NODE_ENV === "development") {
+        url = "http://localhost:5000/article/user/";
+      } else {
+        url = "article/user/";
+      }
+      axios.get(url + this.$store.state.username).then((res) => {
+        let arr = res.data["article_collection"]
+        console.log(arr);
+        this.article_list = arr;
+      }).catch((err) => console.log(err));
+    }
 }
 </script>
