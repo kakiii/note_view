a1db754a77086cb90a3970b8e63a05f037e5eb66
Ellennan <Ellennan@users.noreply.github.com>
2021-04-23 20:36:48 +0100

    TodoList UI

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -2,75 +2,144 @@
 
 <template>
   <div>
-    <!--测试用
+    <!-- 测试用
     <p>Local Data = {{ st }}</p>
-    <button @click="myGetFunction()">Get Local Storage Data</button><br />
--->
+    <button @click="myGetFunction()">Get Local Storage Data</button><br /> -->
+
     <el-container>
+      <!-- <el-header height="20">
+        <h1>To Do List</h1>
+      </el-header> -->
       <el-main>
-        <h2>To Do List</h2>
+        <h1>To Do List</h1>
         <el-input
-            type="text"
-            v-model="inputValue"
-            placeholder="Add ToDo"
-            @keyup.enter="add()"
+          type="text"
+          v-model="inputValue"
+          placeholder="Input ToDo"
+          @keyup.enter.native="add()"
+          style="width: 400px"
         ></el-input>
-
         <el-button type="send" @click="add()">add</el-button>
+
+        <el-row gutter="20">
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <h3>ToDo</h3>
+              <ul v-for="(item, index) in list" :key="'info-' + index">
+                <!-- 这里的info没什么意义就是防俩v-for并行报错 -->
+                <li v-if="item.done === false">
+                  <div style="float: left">
+                    <el-checkbox type="checkbox" @change="change(index, true)">
+                    </el-checkbox>
+                    <span>{{ item.text }}</span>
+                  </div>
+                  <div style="float: right; margin-right: 40px">
+                    <el-button
+                      icon="el-icon-delete"
+                      size="mini"
+                      circle
+                      @click="remove(index)"
+                    ></el-button>
+                  </div>
+                  <br />
+                </li>
+              </ul>
+            </div>
+          </el-col>
+
+          <el-col :span="12">
+            <div class="grid-content bg-purple">
+              <h3>Done</h3>
+              <div>
+                <ul v-for="(item, index) in list" :key="index">
+                  <li v-if="item.done === true">
+                    <div style="float: left">
+                      <el-checkbox
+                        type="checkbox"
+                        @change="change(index, false)"
+                        checked="checked"
+                      ></el-checkbox>
+                      <span>{{ item.text }}</span>
+                    </div>
+                    <div style="float: right; margin-right: 40px">
+                      <el-button
+                        icon="el-icon-delete"
+                        size="mini"
+                        circle
+                        @click="remove(index)"
+                      ></el-button>
+                    </div>
+                    <br />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+
+        <!-- 折叠
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-collapse>
+              <el-collapse-item title="Todo">
+                <ul v-for="(item, index) in list" :key="'info-' + index">
+                  <li v-if="item.done === false">
+                    <div style="float: left">
+                      <el-checkbox
+                        type="checkbox"
+                        @change="change(index, true)"
+                      >
+                      </el-checkbox>
+                      <span>{{ item.text }}</span>
+                    </div>
+                    <div style="float: right; margin-right: 40px">
+                      <el-button
+                        icon="el-icon-delete"
+                        size="mini"
+                        circle
+                        @click="remove(index)"
+                      ></el-button>
+                    </div>
+                    <br />
+                  </li>
+                </ul>
+              </el-collapse-item>
+              <el-collapse-item title="Done">
+                <div>
+                  <ul v-for="(item, index) in list" :key="index">
+                    <li v-if="item.done === true">
+                      <div style="float: left">
+                        <el-checkbox
+                          type="checkbox"
+                          @change="change(index, false)"
+                          checked="checked"
+                        ></el-checkbox>
+                        <span>{{ item.text }}</span>
+                      </div>
+                      <div style="float: right; margin-right: 40px">
+                        <el-button
+                          icon="el-icon-delete"
+                          size="mini"
+                          circle
+                          @click="remove(index)"
+                        ></el-button>
+                      </div>
+                      <br />
+                    </li>
+                  </ul>
+                </div>
+              </el-collapse-item>
+            </el-collapse>
+          </el-col>
+        </el-row> -->
       </el-main>
-    </el-container>
-
-    <el-container>
-      <el-col :span="12"
-      >
-        <div class="grid-content bg-purple">
-
-          <h3>ToDo</h3>
-          <ul v-for="(item, index) in list" :key="'info-' + index">
-            <!-- 这里的info没什么意义就是防俩v-for并行报错-->
-
-            <li v-if="item.done === false">
-              <el-checkbox
-                  type="checkbox"
-                  @change="change(index, true)"
-              ></el-checkbox>
-
-              <span>{{ item.text }}</span>
-              <el-button type="danger" icon="el-icon-delete" circle @click="remove(index)"></el-button>
-
-            </li>
-          </ul>
-        </div>
-      </el-col
-      >
-
-      <el-col :span="12"
-      >
-        <div class="grid-content bg-purple-light">
-          <h3>Done</h3>
-          <ul v-for="(item, index) in list" :key="index">
-            <li v-if="item.done === true">
-              <el-checkbox
-                  type="checkbox"
-                  @change="change(index, false)"
-                  checked="checked"
-
-              ></el-checkbox>
-
-              <span>{{ item.text }}</span>
-
-              <el-button type="danger" icon="el-icon-delete" circle @click="remove(index)"></el-button>
-            </li>
-          </ul>
-        </div>
-      </el-col
-      >
     </el-container>
   </div>
 </template>
 
 <script>
-import store from "../store"
+import store from "../store";
+
 function set(key, val) {
   localStorage.setItem(key, JSON.stringify(val));
 }
@@ -83,91 +152,71 @@ var myStorage = {
   set: set,
   get: get,
 };
-export default {
 
+export default {
   name: "todo",
-  beforeRouteEnter(to,from,next){
-    if(store.state.isLogin != true){
-      next('/login')
-    }else{
+  beforeRouteEnter(to, from, next) {
+    if (store.state.isLogin != true) {
+      next("/login");
+    } else {
       next();
     }
   },
-data()
-{
-  return {
-    inputValue: "",
-    list: [],
-    st: "",
+  data() {
+    return {
+      inputValue: "",
+      list: [],
+      st: "",
 
-    //mylist: localStorage.getItem("myitem"),
-  };
-}
-,
-methods: {
-  add()
-  {
-    if (this.inputValue == null) {
-      alert("不能爲空");
-    } else {
-      //this.list.push({ text: this.inputValue, done: false });
-      this.list.push({text: this.inputValue, done: false});
-
-      this.inputValue = null;
-    }
-  }
-,
-  change(index, done)
-  {
-    if (done) {
-      this.list[index].done = true;
-    } else {
-      this.list[index].done = false;
-    }
-  }
-,
-  remove(index)
-  {
-    this.list.splice(index, 1);
-  }
-,
-
-  /*测试用
-  myGetFunction: function () {
-    this.st = myStorage.get("list");
-    console.log("hello" + this.st);
-  },*/
-}
-,
-
-watch: {
-  list: {
-    deep: true,
-        handler
-  :
-
-    function (newVal, oldVal) {
-      if (newVal) {
-        myStorage.set("list", newVal);
+      //mylist: localStorage.getItem("myitem"),
+    };
+  },
+  methods: {
+    add() {
+      if (this.inputValue == null) {
+        alert("不能爲空");
       } else {
-        myStorage.set("list", oldVal);
-      }
-    }
+        //this.list.push({ text: this.inputValue, done: false });
+        this.list.push({ text: this.inputValue, done: false });
 
-  ,
-  }
-,
-}
-,
-mounted: function () {
-  this.list = myStorage.get("list") || [];
-}
-,
-}
-;
+        this.inputValue = null;
+      }
+    },
+    change(index, done) {
+      if (done) {
+        this.list[index].done = true;
+      } else {
+        this.list[index].done = false;
+      }
+    },
+    remove(index) {
+      this.list.splice(index, 1);
+    },
+    /*测试用
+    myGetFunction: function () {
+      this.st = myStorage.get("list");
+      console.log("hello" + this.st);
+    },*/
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler: function (newVal, oldVal) {
+        if (newVal) {
+          myStorage.set("list", newVal);
+        } else {
+          myStorage.set("list", oldVal);
+        }
+      },
+    },
+  },
+  mounted: function () {
+    this.list = myStorage.get("list") || [];
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .el-main {
   background-color: #e9eef3;
   color: #333;
@@ -175,45 +224,46 @@ mounted: function () {
   line-height: 30px;
 }
 
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-row {
-  margin-bottom: 20px;
-
-&
-:last-child {
-  margin-bottom: 0;
-}
-
-}
 .el-col {
   border-radius: 4px;
 }
 
-.bg-purple-dark {
-  background: #99a9bf;
+.grid-content {
+  border-radius: 5px;
+  min-height: 36px;
 }
 
 .bg-purple {
   background: #d3dce6;
 }
 
-.bg-purple-light {
-  background: #e5e9f2;
+/* .bg-purple-dark {
+  background: #99a9bf;
 }
 
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+.bg-purple-light {
+  background: #e5e9f2;
+} */
+
+ul {
+  list-style: none;
 }
 
 .row-bg {
   padding: 10px 0;
   background-color: #f9fafc;
 }
+
+/* .left {
+  position: absolute;
+  left: 0;
+  width: 100px;
+  padding: 10px;
+}
+.right {
+  position: absolute;
+  right: 0px;
+  width: 100px;
+  padding: 10px;
+} */
 </style>
