70354aea9ca932d18fdb600bf21c07f0d21669ec
Zhong Yiyu <53254770+zhong-yi-yu@users.noreply.github.com>
2021-04-28 15:30:19 +0100

    update discussion latest index

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask.globals import request
 import pymongo
 import datetime
-from flask import Flask, jsonify,render_template,json
+from flask import Flask, jsonify, render_template, json
 
 from auth.user_management import auth
 from content.article import article
@@ -38,38 +38,24 @@ def return_discussion(discussion_id):
         return jsonify({
             'id': discussion_id, 'content': 'NOT FOUND'
         })
-@app.route('/content/discussion',methods=['GET'])
+
+
+@app.route('/content/discussion', methods=['GET'])
 def return_discussion_general():
     discussions = database.discussion
     counts = discussions.find().count()
-    return jsonify({'discussion_size':counts})
+    return jsonify({'discussion_size': counts})
 
 
-@app.route('/content/discussion/latest',methods=['GET'])
-def return_discussion_batch():
+@app.route('/content/discussion/latest', methods=['GET'])
+def return_latest_index():
     discussions = database.discussion
-    col = discussions.find().sort([('timestamp',pymongo.ASCENDING)]).limit(1)
-    collects = []
-    for s in col:
-        collects.append(s)
-    print("\n")
-    print("\n")
-    print("\n")
-    print("\n")
-    print(collects[0])
-    return jsonify({'author':collects[0]['author'],'content':collects[0]['content']})
+    size = discussions.count()
+    return jsonify({'latest':size})
 
 
-@app.route('/content/discussion',methods=["POST"])
-def insert_discussion():
-    discussions = database.discussion
-    request_data = request.get_data()
-    data_json = json.loads(request_data)
-    print(data_json)
-    discussions.insert_one({'author':data_json['author'],
-    'timestamp':datetime.datetime.utcnow(),
-    'content':data_json['content']})
-    print("GET ONE")
+# @app.route('/content/discussion', methods=["POST"])
+# def insert_discussion():
 
 
 if __name__ == "__main__":
