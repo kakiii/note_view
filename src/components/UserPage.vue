4bcb5c03ca5140ca9d59739916e7912c90245dbc
Ellennan <Ellennan@users.noreply.github.com>
2021-04-30 10:08:19 +0100

    Add change avatar function

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,31 +1,38 @@
-<!-- todolist试做,需要优化ui。checkbox问题解决，本地存储todolist-->
-
 <template>
   <div>
     <p>Hello, {{ this.$store.state.username }}!</p>
 
-    <Gravatar :gen_key=this.$store.state.username
-              style="margin: auto"/>
+    <!-- <Gravatar :gen_key="this.$store.state.username" style="margin: auto" /> -->
 
-    <input type="button" value="GET ARTCILE" v-on:click="get_my_article"/>
+    <p v-if="!this.$store.state.gravatar">
+      <Gravatar :gen_key="this.$store.state.username" mode="userpage" />
+    </p>
+    <p v-else>
+      <GravatarsSVG :gen_key="this.$store.state.username" mode="userpage"/>
+    </p>
+
+    <input type="button" value="GET ARTCILE" v-on:click="get_my_article" />
+    <input type="button" value="Change gravatar" v-on:click="changeGravatar" />
     <p>User's article collection in array form.</p>
-<!--    之后其他人把这个东西改成v-for的形式-->
-<!--    等待增加具体的美化-->
-<!--    使用mounted，在user page加载的时候就能自动获取后端用户的数据-->
+    <!--    之后其他人把这个东西改成v-for的形式-->
+    <!--    等待增加具体的美化-->
+    <!--    使用mounted，在user page加载的时候就能自动获取后端用户的数据-->
 
-    <p>{{article_list}}</p>
+    <p>{{ article_list }}</p>
   </div>
 </template>
 
 <script>
 import Gravatar from "./Gravatar.vue";
+import GravatarsSVG from "./Gravatar_SVG.vue";
 import axios from "axios";
+
 export default {
-  components: {Gravatar},
-  data(){
-    return{
-      article_list:[],
-    }
+  components: { Gravatar, GravatarsSVG },
+  data() {
+    return {
+      article_list: [],
+    };
   },
   methods: {
     get_my_article() {
@@ -35,25 +42,39 @@ export default {
       } else {
         url = "article/user/";
       }
-      axios.get(url + this.$store.state.username).then((res) => {
-        let arr = res.data["article_collection"]
-        console.log(arr);
-        this.article_list = arr;
-      }).catch((err) => console.log(err));
-    }
+      axios
+        .get(url + this.$store.state.username)
+        .then((res) => {
+          let arr = res.data["article_collection"];
+          console.log(arr);
+          this.article_list = arr;
+        })
+        .catch((err) => console.log(err));
+    },
+    changeGravatar() {
+      if (this.$store.state.gravatar) {
+        this.$store.state.gravatar = false;
+      } else {
+        this.$store.state.gravatar = true;
+      }
+      console.log("change gravatar");
+    },
   },
   mounted() {
-      let url = "";
-      if (process.env.NODE_ENV === "development") {
-        url = "http://localhost:5000/article/user/";
-      } else {
-        url = "article/user/";
-      }
-      axios.get(url + this.$store.state.username).then((res) => {
-        let arr = res.data["article_collection"]
+    let url = "";
+    if (process.env.NODE_ENV === "development") {
+      url = "http://localhost:5000/article/user/";
+    } else {
+      url = "article/user/";
+    }
+    axios
+      .get(url + this.$store.state.username)
+      .then((res) => {
+        let arr = res.data["article_collection"];
         console.log(arr);
         this.article_list = arr;
-      }).catch((err) => console.log(err));
-    }
-}
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
