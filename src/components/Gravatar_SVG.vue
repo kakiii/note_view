495ff4a79d583b5189518134c1264212c507c625
Ellennan <Ellennan@users.noreply.github.com>
2021-04-30 10:09:19 +0100

    Update Gravatar_SVG.vue

--- src/components/Gravatar_SVG.vue
+++ src/components/Gravatar_SVG.vue
@@ -1,19 +1,13 @@
 <template>
-  <div v-if="size == 4">
+  <div v-if="mode === 'header'">
     <p
-      style="width: 2em; height: 2em; border-radius: 50%; overflow: hidden"
+      style="width: 2.3em; height: 2.3em; border-radius: 50%; overflow: hidden"
       v-html="svgs"
     />
   </div>
-  <div v-else-if="size == 6">
+  <div v-else-if="mode === 'userpage'">
     <p
-      style="width: 3em; height: 3em; border-radius: 50%; overflow: hidden"
-      v-html="svgs"
-    />
-  </div>
-  <div v-else-if="size == 8">
-    <p
-      style="width: 4em; height: 4em; border-radius: 50%; overflow: hidden"
+      style="width: 4em; height: 4em; border-radius: 50%; overflow: hidden;  margin: auto"
       v-html="svgs"
     />
   </div>
@@ -24,15 +18,15 @@ import md5 from "js-md5";
 
 export default {
   name: "GravatarsSVG",
-  props: { gen_key: String, size: Number },
+  props: { gen_key: String, mode: String },
   data() {
     return {
-      svgs: avatar(this.gen_key, this.size),
+      svgs: avatar(this.gen_key, this.mode),
     };
   },
   watch: {
     gen_key(newVal) {
-      this.svgs = avatar(newVal, this.size);
+      this.svgs = avatar(newVal, this.mode);
       // Below is only for Debug use.
       // console.log("Prop changed", newVal, '| was : ',oldVal)
     },
@@ -42,13 +36,19 @@ export default {
   },
 };
 
-function avatar(gen_key, size) {
+function avatar(gen_key, mode) {
   if (gen_key.length == 0) {
     return null;
   }
   var hash = md5(gen_key);
   var str = hash.toString(16);
   var color = "#" + str.substring(0, 6);
+  var size;
+  if (mode == "header") {
+    size = 4;
+  } else if (mode == "userpage") {
+    size = 8;
+  }
   var svg = "<svg>";
   for (var i = 0; i < 8; i++) {
     for (var j = 0; j < 4; j++) {
