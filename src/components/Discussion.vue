a3899ecb310fdb885d28add4cc41d7791801b909
sfyzsr <56057506+sfyzsr@users.noreply.github.com>
2021-04-30 14:24:02 +0100

    Add files via upload

--- src/components/Discussion.vue
+++ src/components/Discussion.vue
@@ -5,9 +5,9 @@
       <el-header>This is discussion borad. 这是讨论版</el-header>
 
       <el-main>
-      <ul v-for="(item, index) in list" :key="index">
-        <span>{{ item.author + " " + item.content }}</span>
-      </ul>
+        <ul v-for="(item, index) in list" :key="index">
+          <span>{{ item.id + " ::" + item.author + " " + item.content }}</span>
+        </ul>
       </el-main>
 
       <el-pagination
@@ -43,7 +43,7 @@ export default {
     return {
       currentPage: 1,
       pagesize: 8,
-      total: 800,
+      total: 0,
       textarea: "",
       discussionContent: "",
       list: [],
@@ -55,77 +55,293 @@ export default {
       this.pagesize = size;
       console.log(this.pagesize);
     },
+
     handleCurrentChange(currentPage) {
       this.currentPage = currentPage;
       console.log(this.currentPage);
     },
+
     clickSending() {
-      var that = this;
-      console.log("Hello " + that.textarea);
-      this.textarea = "";
+      if (this.textarea == "") {
+        alert("not null");
+      } else {
+        let data = {
+          author: "name", //need get author name api
+          content: this.textarea,
+        };
+        axios
+          .post(
+            "http://127.0.0.1:5000/content/discussion",
+            data
+            // , {header: {
+            //   "Content-Type": "application/json", //如果写成contentType会报错
+            // }}
+          )
+          .then((res) => {
+            console.log("res=>", res);
+          });
+        this.textarea = "";
+      }
+      this.currentPage=1;
     },
   },
+
   watch: {
-    pagesize: function () {
+    currentPage: function () {
       let url = "";
-      if(process.env.NODE_ENV === "development"){
+      if (process.env.NODE_ENV === "development") {
         url = "http://127.0.0.1:5000/";
-      }else{
-        url = "/"
+      } else {
+        url = "/";
       }
+
       this.list = [];
       var axiosList = [];
-      var i;
-      for (i = 1; i <= this.pagesize; i++) {
-        axiosList.push(
-          axios.get(url + "content/discussion/" + i)
-        );
+
+      let currentPos = this.total - (this.currentPage-1) * 8;
+      if (currentPos > 8) {
+        for (let i = currentPos; i >= currentPos-7; i--) {
+          axiosList.push(axios.get(url + "content/discussion/" + i));
+        }
+      }else{
+        // currentPos=currentPos+8;
+        for (let i = currentPos; i >= 1; i--) {
+          axiosList.push(axios.get(url + "content/discussion/" + i));
+        }
       }
+
       axios
         .all(axiosList)
         .then((results) => {
           let temp = results.map((r) => r.data);
           for (let j = 0; j < temp.length; j++) {
-              this.list.push({
-                author: temp[j].author,
-                content: temp[j].content,
-              });
+            this.list.push({
+              author: temp[j].author,
+              content: temp[j].content,
+              id: temp[j].id,
+            });
           }
         })
         .catch((err) => console.log(err));
     },
   },
+
+  // created() {
+  //   let url = "";
+  //   if (process.env.NODE_ENV === "development") {
+  //     url = "http://127.0.0.1:5000/";
+  //   } else {
+  //     url = "/";
+  //   }
+
+  //   console.log("start get");
+  //   setTimeout(function () {
+  //   axios
+  //     .get(url + "content/discussion")
+  //     .then((res) => {
+  //       console.log("hello" + res.data["discussion_size"]);
+  //       this.total = res.data["discussion_size"];
+  //       // i = this.total;
+  //       console.log("total is" + this.total);
+  //     })
+  //     .catch((err) => console.log(err))},1000);
+  // },
+
   mounted() {
-          let url = "";
-      if(process.env.NODE_ENV === "development"){
-        url = "http://127.0.0.1:5000/";
-      }else{
-        url = "/"
-      }
-    var axiosList = [];
-    var i;
-    for (i = 1; i <= this.pagesize; i++) {
-      axiosList.push(
-        axios.get(url + "content/discussion/" + i)
-      );
+    let url = "";
+    if (process.env.NODE_ENV === "development") {
+      url = "http://127.0.0.1:5000/";
+    } else {
+      url = "/";
     }
+
     axios
-      .all(axiosList)
-      .then((results) => {
-        let temp = results.map((r) => r.data);
-        // console.log("hello" + temp[1]);
-        // console.log(temp[0].author);
-        for (let j = 0; j < temp.length; j++) {
-            this.list.push({
-              author: temp[j].author,
-              content: temp[j].content,
-            });
-          
+      .get(url + "content/discussion")
+      .then((res) => {
+        console.log("hello" + res.data["discussion_size"]);
+        this.total = res.data["discussion_size"];
+        // i = this.total;
+        console.log("total is" + this.total);
+
+        if (this.total !== 0) {
+          let i = 0;
+          i = this.total;
+          var axiosList = [];
+
+          console.log("already loading " + i);
+
+          if (i > 8) {
+            for (; i >= this.total - 7; i--) {
+              axiosList.push(axios.get(url + "content/discussion/" + i));
+            }
+          } else {
+            for (; i >= 1; i--) {
+              axiosList.push(axios.get(url + "content/discussion/" + i));
+            }
+          }
+
+          axios
+            .all(axiosList)
+            .then()
+            .then((results) => {
+              let temp = results.map((r) => r.data);
+              console.log("hello" + temp[1]);
+              // console.log(temp[0].author);
+              for (let j = 0; j < temp.length; j++) {
+                this.list.push({
+                  author: temp[j].author,
+                  content: temp[j].content,
+                  id: temp[j].id,
+                });
+              }
+            })
+            .catch((err) => console.log(err));
         }
       })
       .catch((err) => console.log(err));
+
+    // let i = 0;
+    // i = this.total;
+    // var axiosList = [];
+
+    // console.log("already loading " + i);
+
+    // for (; i >= i - 7; i--) {
+    //   axiosList.push(axios.get(url + "content/discussion/" + i));
+    // }
+
+    // for (; i >= 1; i--) {
+    //   axiosList.push(axios.get(url + "content/discussion/" + i));
+    // }
+
+    // axios
+    //   .all(axiosList)
+    //   .then()
+    //   .then((results) => {
+    //     let temp = results.map((r) => r.data);
+    //     console.log("hello" + temp[1]);
+    //     // console.log(temp[0].author);
+    //     for (let j = 0; j < temp.length; j++) {
+    //       this.list.push({
+    //         author: temp[j].author,
+    //         content: temp[j].content,
+    //         id: temp[j].id,
+    //       });
+    //     }
+    //   })
+    //   .catch((err) => console.log(err));
   },
 };
+
+//装载总条目至页数
+
+// var async = require("async");
+// async.waterfall(
+//   [
+//     function (callback) {
+//       // let i = 0;
+//       // var axiosList = [];
+//       axios
+//         .get(url + "content/discussion")
+//         .then((res) => {
+//           console.log("hello" + res.data["discussion_size"]);
+//           this.total = res.data["discussion_size"];
+//           // i = this.total;
+//           console.log("Can n i " + this.total);
+
+// if (i >= 8) {
+//   for (; i >= i - 7; i--) {
+//     axiosList.push(axios.get(url + "content/discussion/" + i));
+//   }
+// } else {
+//   for (; i >= 1; i--) {
+//     axiosList.push(axios.get(url + "content/discussion/" + i));
+//   }
+// }
+// })
+//     .catch(() => console.log("Can not get size"));
+//   callback(null, this.total);
+// },
+// function (i, callback) {
+
+//   },
+// ],
+// function (err, results) {
+//   //如果有error则执行此处函数
+//   console.log("异常处理");
+// }
+// );
+
+// let i = 0;
+// var axiosList = [];
+// axios
+//   .get(url + "content/discussion")
+//   .then((res) => {
+//     console.log(res.data["discussion_size"]);
+//     this.total = res.data["discussion_size"];
+//     i = this.total;
+//     console.log("Can n i " + i);
+
+//     if (i >= 8) {
+//       for (; i >= i - 7; i--) {
+//         axiosList.push(axios.get(url + "content/discussion/" + i));
+//       }
+//     } else {
+//       for (; i >= 1; i--) {
+//         axiosList.push(axios.get(url + "content/discussion/" + i));
+//       }
+//     }
+//     axios
+//       .all(axiosList).then()
+//       .then((results) => {
+//         let temp = results.map((r) => r.data);
+//         // console.log("hello" + temp[1]);
+//         // console.log(temp[0].author);
+//         for (let j = 0; j < temp.length; j++) {
+//           this.list.push({
+//             author: temp[j].author,
+//             content: temp[j].content,
+//             id: temp[j].id
+//           });
+//         }
+//       })
+//       .catch((err) => console.log(err));
+//   })
+//   .catch(() => console.log("Can not get size"));
+
+// let totaldis = this.total;
+// console.log("Can n "+totaldis);
+//对第一页进行装载
+
+// let i = this.total;
+
+//console.log("******* i "+i);
+// if (i >= 8) {
+//   for (; i >= i - 7; i--) {
+//     axiosList.push(axios.get(url + "content/discussion/" + i));
+//   }
+// } else {
+//   for (; i >= 1; i--) {
+//     axiosList.push(axios.get(url + "content/discussion/" + i));
+//   }
+// }
+
+// axios
+//   .all(axiosList)
+//   .then((results) => {
+//     let temp = results.map((r) => r.data);
+//     // console.log("hello" + temp[1]);
+//     // console.log(temp[0].author);
+//     for (let j = 0; j < temp.length; j++) {
+//       this.list.push({
+//         author: temp[j].author,
+//         content: temp[j].content,
+//       });
+//     }
+//   })
+//   .catch((err) => console.log(err));
+// },
+// };
 </script>
 
 <style>
