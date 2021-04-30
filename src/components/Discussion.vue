51dabb2c2618a55f9c109e6fc2686d3cdb25d582
Ellennan <Ellennan@users.noreply.github.com>
2021-04-30 14:55:43 +0100

    Modify pagination

--- src/components/Discussion.vue
+++ src/components/Discussion.vue
@@ -11,12 +11,10 @@
       </el-main>
 
       <el-pagination
-        @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="currentPage"
         :page-size="pagesize"
-        :page-sizes="[8, 10, 20, 40]"
-        layout="total, sizes, prev, pager, next, jumper"
+        layout="total, prev, pager, next, jumper"
         :total="total"
       >
       </el-pagination>
@@ -51,11 +49,6 @@ export default {
   },
 
   methods: {
-    handleSizeChange(size) {
-      this.pagesize = size;
-      console.log(this.pagesize);
-    },
-
     handleCurrentChange(currentPage) {
       this.currentPage = currentPage;
       console.log(this.currentPage);
@@ -126,27 +119,6 @@ export default {
     },
   },
 
-  // created() {
-  //   let url = "";
-  //   if (process.env.NODE_ENV === "development") {
-  //     url = "http://127.0.0.1:5000/";
-  //   } else {
-  //     url = "/";
-  //   }
-
-  //   console.log("start get");
-  //   setTimeout(function () {
-  //   axios
-  //     .get(url + "content/discussion")
-  //     .then((res) => {
-  //       console.log("hello" + res.data["discussion_size"]);
-  //       this.total = res.data["discussion_size"];
-  //       // i = this.total;
-  //       console.log("total is" + this.total);
-  //     })
-  //     .catch((err) => console.log(err))},1000);
-  // },
-
   mounted() {
     let url = "";
     if (process.env.NODE_ENV === "development") {
@@ -199,152 +171,11 @@ export default {
         }
       })
       .catch((err) => console.log(err));
-
-    // let i = 0;
-    // i = this.total;
-    // var axiosList = [];
-
-    // console.log("already loading " + i);
-
-    // for (; i >= i - 7; i--) {
-    //   axiosList.push(axios.get(url + "content/discussion/" + i));
-    // }
-
-    // for (; i >= 1; i--) {
-    //   axiosList.push(axios.get(url + "content/discussion/" + i));
-    // }
-
-    // axios
-    //   .all(axiosList)
-    //   .then()
-    //   .then((results) => {
-    //     let temp = results.map((r) => r.data);
-    //     console.log("hello" + temp[1]);
-    //     // console.log(temp[0].author);
-    //     for (let j = 0; j < temp.length; j++) {
-    //       this.list.push({
-    //         author: temp[j].author,
-    //         content: temp[j].content,
-    //         id: temp[j].id,
-    //       });
-    //     }
-    //   })
-    //   .catch((err) => console.log(err));
   },
 };
-
-//装载总条目至页数
-
-// var async = require("async");
-// async.waterfall(
-//   [
-//     function (callback) {
-//       // let i = 0;
-//       // var axiosList = [];
-//       axios
-//         .get(url + "content/discussion")
-//         .then((res) => {
-//           console.log("hello" + res.data["discussion_size"]);
-//           this.total = res.data["discussion_size"];
-//           // i = this.total;
-//           console.log("Can n i " + this.total);
-
-// if (i >= 8) {
-//   for (; i >= i - 7; i--) {
-//     axiosList.push(axios.get(url + "content/discussion/" + i));
-//   }
-// } else {
-//   for (; i >= 1; i--) {
-//     axiosList.push(axios.get(url + "content/discussion/" + i));
-//   }
-// }
-// })
-//     .catch(() => console.log("Can not get size"));
-//   callback(null, this.total);
-// },
-// function (i, callback) {
-
-//   },
-// ],
-// function (err, results) {
-//   //如果有error则执行此处函数
-//   console.log("异常处理");
-// }
-// );
-
-// let i = 0;
-// var axiosList = [];
-// axios
-//   .get(url + "content/discussion")
-//   .then((res) => {
-//     console.log(res.data["discussion_size"]);
-//     this.total = res.data["discussion_size"];
-//     i = this.total;
-//     console.log("Can n i " + i);
-
-//     if (i >= 8) {
-//       for (; i >= i - 7; i--) {
-//         axiosList.push(axios.get(url + "content/discussion/" + i));
-//       }
-//     } else {
-//       for (; i >= 1; i--) {
-//         axiosList.push(axios.get(url + "content/discussion/" + i));
-//       }
-//     }
-//     axios
-//       .all(axiosList).then()
-//       .then((results) => {
-//         let temp = results.map((r) => r.data);
-//         // console.log("hello" + temp[1]);
-//         // console.log(temp[0].author);
-//         for (let j = 0; j < temp.length; j++) {
-//           this.list.push({
-//             author: temp[j].author,
-//             content: temp[j].content,
-//             id: temp[j].id
-//           });
-//         }
-//       })
-//       .catch((err) => console.log(err));
-//   })
-//   .catch(() => console.log("Can not get size"));
-
-// let totaldis = this.total;
-// console.log("Can n "+totaldis);
-//对第一页进行装载
-
-// let i = this.total;
-
-//console.log("******* i "+i);
-// if (i >= 8) {
-//   for (; i >= i - 7; i--) {
-//     axiosList.push(axios.get(url + "content/discussion/" + i));
-//   }
-// } else {
-//   for (; i >= 1; i--) {
-//     axiosList.push(axios.get(url + "content/discussion/" + i));
-//   }
-// }
-
-// axios
-//   .all(axiosList)
-//   .then((results) => {
-//     let temp = results.map((r) => r.data);
-//     // console.log("hello" + temp[1]);
-//     // console.log(temp[0].author);
-//     for (let j = 0; j < temp.length; j++) {
-//       this.list.push({
-//         author: temp[j].author,
-//         content: temp[j].content,
-//       });
-//     }
-//   })
-//   .catch((err) => console.log(err));
-// },
-// };
 </script>
 
-<style>
+<style scoped>
 .el-header {
   background-color: #161515;
   color: rgb(255, 0, 0);
@@ -365,7 +196,4 @@ export default {
   text-align: center;
   line-height: 60px;
 }
-</style>
-
-<style scoped>
 </style>
