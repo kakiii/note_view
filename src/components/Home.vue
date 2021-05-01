2e63c58413d12b84cc2a8a1571eee8cd45bbf1d1
kaki <43228828+kakiii@users.noreply.github.com>
2021-05-01 21:56:46 +0100

    add explore functionality in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,46 @@
 <template>
   <div>
     <h1>HOME PAGE</h1>
-
+    <ul>
+      <li v-for="article in articles" :key="article">{{article.Title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Home",
-  methods:{
-
-  }
+  mounted() {
+    let url = "";
+    if (process.env.NODE_ENV === "development") {
+      url = "http://localhost:5000/article/";
+    } else {
+      url = "/article/";
+    }
+    this.random_article_ids = axios
+      .get(url + "random")
+      .then((res) => {
+        this.random_article_ids = res.data["id"];
+        let articles_get = [];
+        this.random_article_ids.forEach((id) => {
+          articles_get.push(axios.get(url + id));
+        });
+        axios.all(articles_get).then((res) => {
+          this.articles = res.map((r) => r.data);
+          console.log(this.articles);
+        });
+        //console.log(this.random_article_ids);
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {},
+  data() {
+    return {
+      random_article_ids: [],
+      articles: [],
+    };
+  },
 };
 </script>
 
